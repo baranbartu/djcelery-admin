f26cd5b4c54351b44878a5629501ff833bdfafdd
Baran Bartu Demirci <baranbartu@Barans-MacBook-Air.local>
2015-11-19 00:21:59 +0200

    updated project structure

--- celeryadmin/views.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'baranbartu'
-
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
-
-
-def celery_monitoring(request):
-    return render_to_response('celery-monitoring.html', {},
-                              RequestContext(request))
--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'celeryadmin'
+    'celeryadmin',
 )
 
 MIDDLEWARE_CLASSES = (
