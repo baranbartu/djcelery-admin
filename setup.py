48a92fe88824d9161882165fd8049fabb4efd83c
Baran Bartu Demirci <baranbartu@Barans-MacBook-Air.local>
2015-11-18 17:22:11 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+try:
+    README = open('README').read()
+except:
+    README = None
+
 setup(
     name='djcelery-admin',
     packages=find_packages(exclude=['sample_project']),
     version='0.0.1',
     url='http://github.com/baranbartu/djcelery-admin',
     license='MIT',
+    long_description=README,
     author='Baran Bartu Demirci',
     author_email='bbartu.demirci@gmail.com',
     description='Celery task monitoring and management on custom admin page',
