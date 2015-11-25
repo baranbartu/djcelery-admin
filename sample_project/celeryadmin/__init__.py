83a2992d35fa85488f80a975dd271c24d9170055
Baran Bartu Demirci <baranbartu@Barans-MacBook-Air.local>
2015-11-26 00:51:54 +0200

    event_listener initialization

--- sample_project/celeryadmin/__init__.py
+++ sample_project/celeryadmin/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'baranbartu'
+from client import CeleryClient
+from context import ContextManager
+from events import EventListener
+
+celery_client = CeleryClient()
+context_manager = ContextManager(client=celery_client)
+event_listener = EventListener(celery_client, context_manager)
+event_listener.start()
